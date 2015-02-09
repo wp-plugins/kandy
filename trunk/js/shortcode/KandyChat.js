5e6a0103a8661a7b707a7f836a456f060e568743
kodeplusdev <kodeplusdev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-09 14:47:15 +0000

    Updating tag to 1.4
    git-svn-id: https://plugins.svn.wordpress.org/kandy/tags/1.4@1085588 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/js/shortcode/KandyChat.js
@@ -0,0 +1,14 @@
+/**
+ * You login successfully
+ */
+window.loginsuccess_callback = function () {
+   //do something here
+}
+
+/**
+ * You login fail
+ */
+window.loginfailed_callback = function () {
+   //do something here
+}
+
