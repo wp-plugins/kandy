c300a3717a472628a23c32cbbc2da196a829ddbf
kodeplusdev <kodeplusdev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-09 14:46:50 +0000

    remove js callback
    git-svn-id: https://plugins.svn.wordpress.org/kandy/trunk@1085587 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/shortcode/KandyChat.js
+++ js/shortcode/KandyChat.js
@@ -2,27 +2,13 @@
  * You login successfully
  */
 window.loginsuccess_callback = function () {
-    changeUIState('LOGGED_IN');
+   //do something here
 }
 
 /**
  * You login fail
  */
 window.loginfailed_callback = function () {
-
-}
-
-window.changeUIState = function(state) {
-    switch (state) {
-        case 'LOGGED_OUT':
-            $('.kandyChat .kandyMessages').empty();
-            emptyContact();
-
-            break;
-        case 'LOGGED_IN':
-            $("#loading").hide();
-            $("#chat-wrapper").show();
-            break;
-    }
+   //do something here
 }
 
