194f31d542bcdeb2a747672c7d8d10837fb59e51
kodeplusdev <kodeplusdev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-10 02:57:54 +0000

    Updating tag to 1.4
    git-svn-id: https://plugins.svn.wordpress.org/kandy/tags/1.4@1086064 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/js/shortcode/KandyVoice.js
@@ -0,0 +1,73 @@
+/**
+ * You login success
+ */
+window.loginsuccess_callback = function () {
+    //do something here
+}
+
+/**
+ * you login fail
+ */
+window.loginfailed_callback = function () {
+//do something here
+}
+
+/**
+ * Someone are calling you
+ * @param call
+ * @param isAnonymous
+ */
+window.callincoming_callback = function (call, isAnonymous) {
+    //do something here
+}
+
+/**
+ * You are on call
+ * @param call
+ */
+window.oncall_callback = function (call) {
+    //do something here
+}
+/**
+ * Some one answer your call
+ * @param call
+ * @param isAnonymous
+ */
+window.callanswered_callback = function (call, isAnonymous) {
+    //do something here
+}
+
+/**
+ * end call callback
+ */
+window.callended_callback = function () {
+    //do something here
+}
+/*
+*   Callback when click AnswerVideo Button
+ */
+window.answerVideoCall_callback = function (stage) {
+    //do something here
+}
+
+/*
+ *   Callback when click AnswerVideo Button
+ */
+window.answerVoiceCall_callback = function (stage) {
+    //do something here
+}
+
+/*
+ *   Callback when click Call Button
+ */
+window.makeCall_callback = function (stage) {
+    //do something here
+}
+
+/*
+ *   Callback when click End call Button
+ */
+window.endCall_callback = function (stage) {
+    //do something here
+}
+
